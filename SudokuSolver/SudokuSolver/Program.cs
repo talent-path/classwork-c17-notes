a2c2dc58ef7121693563d4fb0cd5fbbd2c95c3ba
dsmelser <dsmelser.personal@gmail.com>
2021-05-25 09:16:44 -0500

    finished sudoku solver

--- SudokuSolver/SudokuSolver/Program.cs
+++ SudokuSolver/SudokuSolver/Program.cs
@@ -8,36 +8,114 @@ namespace SudokuSolver
         {
 
             int[,] vals = {
-                { 0, 5, 0, 3, 4, 7, 1, 9, 0 },
-                { 0, 0, 7, 0, 0, 0, 0, 6, 0 },
-                { 2, 9, 0, 8, 6, 0, 7, 5, 0 },
-                { 4, 0 ,0, 0, 0, 6, 0, 3, 0 },
-                { 0, 0, 0, 1, 9, 4, 0, 0, 0 },
-                { 0, 6, 0, 2, 0, 0, 0, 0, 5 },
-                { 0, 2, 5, 0, 1, 0, 0, 4, 9 },
-                { 0, 1, 0, 0, 0, 0, 2, 0, 0 },
-                { 0, 4, 9, 6, 3, 2, 0, 1, 0 }
+                { 1, 0, 0, 0, 0, 9, 5, 0, 7 },
+                { 0, 0, 0, 0, 4, 0, 0, 0, 0 },
+                { 7, 0, 8, 0, 0, 0, 0, 0, 2 },
+                { 2, 0 ,3, 0, 7, 0, 0, 0, 0 },
+                { 0, 1, 0, 0, 3, 0, 0, 4, 0 },
+                { 0, 0, 0, 0, 9, 0, 7, 0, 1 },
+                { 3, 0, 0, 0, 0, 0, 6, 0, 8 },
+                { 0, 0, 0, 0, 2, 0, 0, 0, 0 },
+                { 5, 0, 2, 6, 0, 0, 0, 0, 3 }
             };
 
             SudokuBoard board = new SudokuBoard(vals);
 
-            bool changedVal = false;
-            for( int row = 0; !changedVal && row < 9; row++)
+            Solve(board);
+        }
+
+        static bool Solve( SudokuBoard board)
+        {
+            //1. Find the square with the fewest choices.
+            //2. If the number of choices is 0, we have to stop (we made
+            //      a bad guess earlier
+            //3. If the number of choices is 1, obviously we just pick it
+            //4. If the number of choise is >1, we'll loop through each possibility
+            //      and try it recursively
+
+
+            int minChoiceRow = -1;
+            int minChoiceCol = -1;
+            int minChoiceCount = int.MaxValue;
+
+
+
+            for (int row = 0; row < 9; row++)
             {
-                for( int col = 0; !changedVal && col < 9; col++)
+                for (int col = 0; col < 9; col++)
                 {
-                    if(board.AllowableVals[row, col] != null && board.AllowableVals[row, col].Count == 1)
+                    if (   board.AllowableVals[row, col] != null
+                        && board.AllowableVals[row, col].Count < minChoiceCount)
                     {
-                        board.SetValue(row, col, board.AllowableVals[row, col][0]);
-                        changedVal = true;
+                        minChoiceCount = board.AllowableVals[row, col].Count;
+                        minChoiceRow = row;
+                        minChoiceCol = col;
                     }
                 }
             }
 
-            Console.WriteLine("Hello World!");
 
-            //I lied (mac version?)
-            //Console.WriteLine("this shouldn't work except VS is great");
+            bool success = false;
+            if( minChoiceCount > 0)
+            {
+                foreach( var allowableVal in board.AllowableVals[minChoiceRow, minChoiceCol])
+                {
+                    //1. try the value
+                    board.SetValue(minChoiceRow, minChoiceCol, allowableVal);
+
+                    //check for success???
+                    success = board.IsComplete;
+
+                    //2. recurse
+                    if (success)
+                    {
+                        //if we're successful here, that means we just filled in
+                        //the last square
+
+                        PrintBoard(board)
+;                    }
+                    else
+                    {
+                        success = Solve(board);
+                        //if we're successful after this line, that means somewhere
+                        ///down the line finished
+                        //aka, we just guessed correctly and verified it works
+                    }
+
+
+                    //maybe don't need because we just iterate to the next possible value
+                    //3. back out our change and continue
+                    if (!success)
+                    {
+                        board.SetValue(minChoiceRow, minChoiceCol, 0);
+                    }
+                    else break;
+                }
+            }
+
+            return success;
+
+            //board.SetValue(row, col, board.AllowableVals[row, col][0]);
+        }
+
+        private static void PrintBoard(SudokuBoard board)
+        {
+            for( int row = 0; row < 9; row++)
+            {
+                if( row % 3 == 0)
+                {
+                    Console.WriteLine("-----------");
+                }
+                for( int col = 0; col < 9; col++)
+                {
+                    if( col %3 == 0)
+                    {
+                        Console.Write("|");
+                    }
+                    Console.Write(board.Vals[row, col]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
