857dec2eb520ac065268fc2ead2dc5c49834bfde
dsmelser <dsmelser.personal@gmail.com>
2021-05-27 09:40:23 -0500

    added streamreader demo

---
+++ StreamReaderDemo/StreamReaderDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace StreamReaderDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../words.txt"))
+            {
+                List<string> linesWithA = new List<string>();
+                string line = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if(line.Contains('b',StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        linesWithA.Add(line);
+                    }
+                }
+                Console.WriteLine(linesWithA.Count + " lines with an 'a' in it.");
+            }
+
+        }
+    }
+}
