64ecc04e9809d1aacf6cb1b1002e6576212c0f52
dsmelser <dsmelser.personal@gmail.com>
2021-06-02 11:14:05 -0500

    missed save

--- GenericsDemo/GenericHeap/MinHeap.cs
+++ GenericsDemo/GenericHeap/MinHeap.cs
@@ -1,10 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace GenericHeap
 {
-    public class Heap
-    {
-        public Heap()
+
+    //what is a heap?
+    //heap property - each "node" has 2 children,
+    //each child is (less for maxheap/greater for minheap) than the parent
+    //  therefore top-most node is largest/smallest, respectively
+
+
+    //parents & children in an array/list
+
+    //                  0
+    //       1                    2
+    //    3     4              5    6
+    //   7 8   9 X
+
+    // LeftChildIndex( int parentIndex ) => parentIndex * 2 + 1;
+    // RightChildIndex( int parentIndex ) => parentIndex * 2 + 2;
+    // ParentIndex( int childIndex ) => (childIndex - 1) / 2;
+
+
+    //https://en.wikipedia.org/wiki/Binary_heap
+    public class MinHeap<T> where T : IComparable<T>
+    { 
+
+        //every incoming object will implement IComparable<T>
+        //CompareTo semantics:
+
+        //this.CompareTo( that )
+        //  -  this comes "before" that or this < that
+        //  0  this == that
+        //  +  this comes "after" that or this > that
+
+        List<T> _allElements = new List<T>();
+
+        public void Add( T toAdd)
         {
+            _allElements.Add(toAdd);
+            SiftUp();
+        }
+
+        public void Remove( T toRemove)
+        {
+            //we're looking for a CompareTo() == 0
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            if (_allElements.Count > 0) return _allElements[0];
+            return default(T);
+        }
+
+        public T Pop()
+        {
+            T toReturn = default(T);
+            if (_allElements.Count > 0) toReturn = _allElements[0];
+
+            //todo: actually remove the top node
+            //swap with the last element
+            //remove the last element
+            //heapify()
+
+            return toReturn;
+        }
+
+        private void SiftUp()
+        {
+            throw new NotImplementedException();
         }
     }
 }
