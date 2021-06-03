b92bee37b7aa184f1c804b6b5774ce77312432d3
dsmelser <dsmelser.personal@gmail.com>
2021-06-03 13:43:13 -0500

    added mutex code

--- AsyncDemo/AsyncDemo/Program.cs
+++ AsyncDemo/AsyncDemo/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsyncDemo
 {
     class Program
     {
+        static Mutex _inputsLock = new Mutex();
+        static Queue<int> _fromValues = new Queue<int>();
+        static Queue<int> _toValues = new Queue<int>();
+
+
+        //static List<int> _results = new List<int>();
+        static Queue<int> _results = new Queue<int>();
+        static Mutex _resultsLock = new Mutex();
+
         static async Task Main(string[] args)
         {
 
@@ -14,11 +24,25 @@ namespace AsyncDemo
 
 
             watch.Start();
-            Task<int> longJob = Task<int>.Factory.StartNew(() => Count(100_000_000, 200_000_000));
-            Task<int> shortJob = Task<int>.Factory.StartNew(() =>  Count(1, 10));
+            //Task<int> longJob = Task<int>.Factory.StartNew(() => Count(100_000_000, 200_000_000));
+            //Task<int> shortJob = Task<int>.Factory.StartNew(() =>  Count(1, 10));
 
-            List<Task<int>> tasks = new List<Task<int>> { longJob, shortJob };
+            //List<Task<int>> tasks = new List<Task<int>> { longJob, shortJob };
 
+
+            List<Task<int>> tasks = new List<Task<int>>();
+
+            for( int thread = 0; thread < 40; thread++)
+            {
+                _inputsLock.WaitOne();
+                _fromValues.Enqueue(RNG.NextInt(10000, 20000));
+                _toValues.Enqueue(RNG.NextInt(30000, 40000));
+                _inputsLock.ReleaseMutex();
+                var jobToAdd = Task<int>.Factory.StartNew(
+                    () => Count());
+
+                tasks.Add(jobToAdd);
+            }
 
             long lastTime = watch.ElapsedMilliseconds;
 
@@ -52,7 +76,7 @@ namespace AsyncDemo
             //now we'll try setting up a task to run when all results are complete
 
             await Task.Factory.ContinueWhenAll(tasks.ToArray(),
-                (allTasks) => Console.WriteLine("we're done"));
+                (allTasks) => Console.WriteLine("we're done: " + watch.ElapsedMilliseconds));
 
 
 
@@ -63,6 +87,11 @@ namespace AsyncDemo
 
             watch.Stop();
 
+            foreach( int result in _results)
+            {
+                Console.WriteLine(result);
+            }
+
 
 
 
@@ -71,19 +100,53 @@ namespace AsyncDemo
         }
 
 
-        static int Count( int from, int to)
+        static int Count()
         {
-            
-            //Console.WriteLine($"counting from {from} to {to}");
-            int sum = 0;
-            for( int i = from; i <= to; i++)
+            int count = 0;
+
+
+            bool inputLocked = _inputsLock.WaitOne();
+            if (inputLocked)
             {
-                sum++;
+                int from = _fromValues.Dequeue();
+                int to = _toValues.Dequeue();
+
+                _inputsLock.ReleaseMutex();
+
+                Console.WriteLine("Starting " + Thread.CurrentThread.Name + $"({from}, {to})");
+
+
+                //Console.WriteLine($"counting from {from} to {to}");
+                for (int i = from; i <= to; i++)
+                {
+                    count++;
+                }
+
+
+                bool securedLock = _resultsLock.WaitOne();
+                if (securedLock)
+                {
+                    _results.Enqueue(count);
+                    _resultsLock.ReleaseMutex();
+                }
+                Console.WriteLine("Stopping " + Thread.CurrentThread.Name + $"({from}, {to})");
+
             }
+
+            //lock( _results)
+            //{
+            //    _results.Add(count);
+            //}
+
+            //this should only happen in this if
+            //otherwise this thread doesn't "own" the mutex
+            //and this line will cause an exception
+
+
 
             //Console.WriteLine($"count from {from} to {to} equals {sum}");
 
-            return sum;
+            return count;
         }
     }
 }
---
+++ AsyncDemo/AsyncDemo/RNG.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AsyncDemo
+{
+    public static class RNG
+    {
+        static Random _rng = new Random();
+
+
+        public static int NextInt( int incMin, int incMax)
+        {
+            return _rng.Next(incMin, incMax + 1);
+        }
+    }
+}
